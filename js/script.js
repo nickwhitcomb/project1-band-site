f300e512156837133b900166e1d83de003bc3b04
nickwhitcomb <you@example.com>
2018-04-04 22:32:42 -0700

    about and js commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('h2').html()
+  });
+
+
+// $(document).ready(function() { 
+//     $(".nav li").on("click", function () {
+//     $(".nav li").removeClass("active");
+//     $(this).addClass("active");
+//     });
+// });
+
+
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("p").slideToggle();
+//     });
+// });
